643112086b3210e2bb5f4d2aadb749d8ad7dbfeb
Fatahillah <Fatahillah@yandex.com>
2026-08-02 14:21:31 +0700

    hardware portfolio hyperlink added

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -5,7 +5,15 @@ import Reveal from "../Reveal/Reveal";
 const Footer = () => {
   return (
     <Reveal as="footer" className="footer">
-      Copyright &#169; 2023 Fatahillah. All right received.
+      <span>Copyright &#169; 2023 Fatahillah. All right received.</span>
+      <a
+        className="footerLink"
+        href="https://sites.google.com/view/fatahproject39/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Engineering & Hardware Projects
+      </a>
     </Reveal>
   );
 };
